c39d89dd0708a96433edf5c0bc4c300cab49cf4c
Johanna-hub <jyherman@gmail.com>
2019-01-15 20:19:36 +0000

    async/await

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+function manualRotation(){
+    alert("Thank you for rotating!")
+}
+
 async function start() {
     try {
-      await screen.orientation.lock('landscape')
-    } catch {
-      alert('To start, rotate your screen')
+        await screen.orientation.lock('landscape')
+        } 
+
+    catch  {
+      alert("To start, rotate your screen to landscape")
+      function orientationChangeHandler() {
+        if (!screen.orientation.type.startsWith('landscape')) {
+          return;
+        }
+        screen.orientation.removeEventListener('change', orientationChangeHandler);
+        manualRotation();
+      }
+      screen.orientation.addEventListener('change', orientationChangeHandler);
     }
   }
 
   window.onload = start;
->>>>>>> try/catch
