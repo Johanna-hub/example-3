987e2ce7a6f8e36154ced5c2518880878892c122
Johanna-hub <jyherman@gmail.com>
2019-01-15 20:41:44 +0000

    Example 3: add button

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function manualRotation() {
-    alert("Thank you for rotating!")
+    alert("Thank you for rotating!");
 }
 
 async function start() {
+    const promiseToFullScreen = new Promise(resolve =>
+        document.addEventListener("fullscreenchange", resolve)
+      );
+    await document.documentElement.requestFullscreen();
+    await promiseToFullScreen;
     try {
-        await screen.orientation.lock('landscape')
+        await screen.orientation.lock('landscape');
     } 
     catch {
-        alert("To start, rotate your screen to landscape")
+        alert("To start, rotate your screen to landscape");
         function orientationChangeHandler() {
             if (!screen.orientation.type.startsWith('landscape')) {
             return;
@@ -18,4 +23,4 @@ async function start() {
       screen.orientation.addEventListener('change', orientationChangeHandler);
     }
 }
-window.onload = start;
+// window.onload = start;
