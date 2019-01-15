d773d06be87ec6c2e2d328255b519cf1b739c1f5
Johanna-hub <jyherman@gmail.com>
2019-01-15 14:32:02 +0000

    Example 3: Test

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+  var start = function() {
+    screen.orientation.lock('portrait').then(
+      function() {
+        alert('To start, rotate your screen to landscape.');
+
+        var orientationChangeHandler = function() {
+          if (!screen.orientation.type.startsWith('portrait')) {
+            return;
+          }
+          screen.orientation.removeEventListener('change', orientationChangeHandler);
+        }
+
+        screen.orientation.addEventListener('change', orientationChangeHandler);
+      })
+  }
+  window.onload = start;
+</script>
