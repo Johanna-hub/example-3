abc335198c8fdd0deb4d94e4d95dad5bcdaf3354
Johanna-hub <jyherman@gmail.com>
2019-01-15 20:26:03 +0000

    Example 3 tidy

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-function manualRotation(){
+function manualRotation() {
     alert("Thank you for rotating!")
 }
 
 async function start() {
     try {
         await screen.orientation.lock('landscape')
-        } 
-
-    catch  {
-      alert("To start, rotate your screen to landscape")
-      function orientationChangeHandler() {
-        if (!screen.orientation.type.startsWith('landscape')) {
-          return;
-        }
+    } 
+    catch {
+        alert("To start, rotate your screen to landscape")
+        function orientationChangeHandler() {
+            if (!screen.orientation.type.startsWith('landscape')) {
+            return;
+            }
         screen.orientation.removeEventListener('change', orientationChangeHandler);
         manualRotation();
-      }
+        }
       screen.orientation.addEventListener('change', orientationChangeHandler);
     }
-  }
-
-  window.onload = start;
+}
+window.onload = start;
