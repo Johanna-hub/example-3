e0572ca73ef495fb1b15071759af672fa5fdf057
Johanna-hub <jyherman@gmail.com>
2019-01-17 12:03:19 +0000

    matchMedia fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function manualRotation() {
 }
 
 function orientationChangeHandler() {
-  if (window.matchMedia("(orientation: portrait)").matches) {
+  if (!window.matchMedia("(orientation: landscape)").matches) {
     return;
   }
   window.removeEventListener("orientationchange", orientationChangeHandler);
@@ -20,10 +20,50 @@ async function start() {
 }
 
 function thanks() {
-  alert("Rotated");
+  alert("Rotate");
   window.removeEventListener("orientationchange", orientationChangeHandler);
 }
 
 function rotate() {
   window.addEventListener("orientationchange", thanks);
 }
+
+function ready() {
+  const { type } = screen.orientation;
+  console.log("Fullscreen and locked to ${type}. Ready!");
+}
+
+async function start() {
+  await document.documentElement.requestFullscreen();
+  await screen.orientation.lock("landscape");
+  ready();
+}
+
+async function fullScreenCheck() {
+  if (document.fullscreenElement === null) {
+    await document.body.requestFullscreen();
+  }
+}
+
+async function unlock() {
+  await fullScreenCheck();
+  await screen.orientation.unlock();
+}
+
+async function rotate() {
+  const rotate = document.getElementById("rotate");
+  await fullScreenCheck();
+  const newOrientation = screen.orientation.type.startsWith("portrait")
+    ? "landscape"
+    : "portrait";
+  await screen.orientation.lock(newOrientation);
+  rotate.textContent = `Rotate to ${newOrientation}`;
+}
+
+function show() {
+  const { type, angle } = screen.orientation;
+  console.log(`Orientation type is ${type} & angle is ${angle}`);
+}
+
+screen.orientation.addEventListener("change", show);
+window.addEventListener("load", show);
