53bc0ed50fb95bf9917ecbb2226d2599e7e91eaf
Johanna-hub <jyherman@gmail.com>
2019-01-22 10:27:51 +0000

    refactored

--- script.js
+++ script.js
@@ -36,3 +36,26 @@ function thanks() {
 function rotate() {
   window.addEventListener("orientationchange", thanks);
 }
+
+function final() {
+  /* Start application when in correct orientation */
+}
+async function start2() {
+  try {
+    await screen.orientation.lock("landscape");
+    final();
+  } catch (err) {
+    console.error(err);
+  }
+  const matchLandscape = matchMedia("(orientation: landscape)");
+  if (matchLandscape.matches) return final(); 
+  addEventListener("orientationchange", function listener() {
+    matchPortrait.addListener(function mediaChange(e) {
+      if (!e.matches) return; // Nope, not landscape!
+      removeEventListener("orientationchange", listener);
+      matchPortrait.removeListener(mediaChange);
+      final();
+    });
+  });
+  alert("To start, please rotate your screen to landscape.");
+}
